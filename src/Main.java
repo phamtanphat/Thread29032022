ba49f11da0abc85822623b0e46b4a5fb99af61bf
Phat <phatdroid94@gmail.com>
2022-06-09 18:37:26 +0700

    manage thread

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,53 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     static int a, b, c;
+    static MyFlag myFlag;
+
     public static void main(String[] args) {
         a = b = c = 0;
+        myFlag = new MyFlag(1);
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 1; i <= 50 ; i++) {
-                    a = i;
-                    System.out.println(String.format("A : %d", a));
+                synchronized (myFlag) {
+                    for (int i = 1; i <= 50; ) {
+                        if (myFlag.index == 1) {
+                            a = i;
+                            System.out.println(String.format("A : %d", a));
+                            myFlag.index = 2;
+                            myFlag.notifyAll();
+                            i++;
+                        } else {
+                            try {
+                                myFlag.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
                 }
+
             }
         });
         Thread threadB = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 1; i <= 50 ; i++) {
-                    b = i;
-                    System.out.println(String.format("B : %d", b));
+                synchronized (myFlag) {
+                    for (int i = 1; i <= 50; ) {
+                       if (myFlag.index == 2) {
+                           b = i;
+                           System.out.println(String.format("B : %d", b));
+                           myFlag.index = 3;
+                           myFlag.notifyAll();
+                           i++;
+                       } else {
+                           try {
+                               myFlag.wait();
+                           } catch (InterruptedException e) {
+                               e.printStackTrace();
+                           }
+                       }
+                    }
                 }
             }
         });
@@ -26,9 +56,22 @@ public class Main {
         Thread threadC = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 1; i <= 50 ; i++) {
-                    c = a + b;
-                    System.out.println(String.format("C : %d", c));
+                synchronized (myFlag) {
+                    for (int i = 1; i <= 50;) {
+                        if (myFlag.index == 3) {
+                            c = a + b;
+                            System.out.println(String.format("C : %d", c));
+                            myFlag.index = 1;
+                            myFlag.notifyAll();
+                            i++;
+                        } else {
+                            try {
+                                myFlag.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
                 }
             }
         });
---
+++ src/MyFlag.java
@@ -0,0 +1,7 @@
+public class MyFlag {
+    int index;
+
+    public MyFlag(int index) {
+        this.index = index;
+    }
+}
