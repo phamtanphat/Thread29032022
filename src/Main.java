a0c6a5517ce9beaa9235c69974c6f026ddd21549
Phat <phatdroid94@gmail.com>
2022-06-09 18:19:29 +0700

    condition

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,40 @@
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    static int a, b, c;
     public static void main(String[] args) {
+        a = b = c = 0;
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
-                printMessage("A");
+                for (int i = 1; i <= 50 ; i++) {
+                    a = i;
+                    System.out.println(String.format("A : %d", a));
+                }
             }
         });
         Thread threadB = new Thread(new Runnable() {
             @Override
             public void run() {
-                printMessage("B");
+                for (int i = 1; i <= 50 ; i++) {
+                    b = i;
+                    System.out.println(String.format("B : %d", b));
+                }
+            }
+        });
+
+        Thread threadC = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 50 ; i++) {
+                    c = a + b;
+                    System.out.println(String.format("C : %d", c));
+                }
             }
         });
 
         threadB.start();
         threadA.start();
-    }
-    private synchronized static void printMessage(String message) {
-        for (int i = 0; i < 50 ; i++) {
-            System.out.println(String.format("%s : %d", message, i));
-        }
+        threadC.start();
     }
 }
